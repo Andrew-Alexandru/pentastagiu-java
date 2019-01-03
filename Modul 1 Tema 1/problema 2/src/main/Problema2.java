3ac9882c87fd79592d7b4cc22fbed0a73d2267f2
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2019-01-03 14:21:09 +0200

    Some changes

--- Modul 1 Tema 1/problema 2/src/main/Problema2.java
+++ Modul 1 Tema 1/problema 2/src/main/Problema2.java
@@ -15,18 +15,18 @@ public class Problema2 {
 
 		Scanner scan = new Scanner(System.in);
 	    System.out.print("Insert first number: ");
-	    int firstNumber = scan.nextInt();
+	    int NumberOne = scan.nextInt();
 	    System.out.print("Insert the second number: ");
-	    int secondNumber = scan.nextInt();
+	    int NumberTwo = scan.nextInt();
 	    int sum = 0;
 	    int counter = 0;
 	    float average = (float) 0.0;
-	    for(int i = firstNumber; i <= secondNumber; i++) {
+	    for(int i = NumberOne; i <= NumberTwo; i++) {
 	    	sum = sum + i;
 	    	counter++;
 	    	average = ((float)sum / counter);
 	    }
-	    System.out.print("Sum for range " +  firstNumber + " to " + secondNumber + " is "+ sum + "\n");
-	    System.out.print("Average for range " +  firstNumber + " to " + secondNumber + " is " + average + "\n");
+	    System.out.print("Sum for range " +  NumberOne + " to " + NumberTwo + " is "+ sum + "\n");
+	    System.out.print("Average for range " +  NumberOne + " to " + NumberTwo + " is " + average + "\n");
 	}
 }
